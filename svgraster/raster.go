0d6044b3358efffaea14717289b179eacbfe6033
Inkeliz <inkeliz@inkeliz.com>
2020-11-18 21:41:57 +0000

    fix issue when viewbox is less than zero

--- svgraster/raster.go
+++ svgraster/raster.go
@@ -54,6 +54,9 @@ func RasterSVGIconToImage(icon io.Reader, scanner rasterx.Scanner) (*image.RGBA,
 		return nil, err
 	}
 	w, h := int(parsedIcon.ViewBox.W), int(parsedIcon.ViewBox.H)
+
+	parsedIcon.SetTarget(0, 0, float64(w), float64(h))
+
 	img := image.NewRGBA(image.Rect(0, 0, w, h))
 
 	if scanner == nil {
